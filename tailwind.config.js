a335f7ff14f8b90ffa6d84e4dc0dcac0c9dcde1e
Javohir0080703 <renobobopocta@gmail.com>
2024-04-19 09:10:18 +0500

    Add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ export default {
   theme: {
     extend: {
       colors:{
-        "#040D12":"#040D12"
+        "#040D12":"#040D12",
+        "#CBC6FD":"#CBC6FD",
+        "#9F95FF":"#9F95FF"
+      },
+      margin:{
+        "26px":"26px"
+      },
+      boxShadow:{
+        "HeaderBtn":"0px 0px 25px 0px rgb(159, 149, 255)"
       }
     },
   },
