38e4e10e5907b66c893e3dd3a6af7eb5392fc772
Javohir0080703 <renobobopocta@gmail.com>
2024-05-09 22:46:50 +0500

    add responsive Rewiews page

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -7,10 +7,10 @@ import { HomeReviewsData } from "../data";
 const Reviews = ({ language, setLanguage }) => {
   const [scroll, setScroll] = useState(false);
   const [iteamsNumber, setIteamsNumber] = useState(2);
-  const handleAdd = ()=>{
-    setScroll(!scroll)
-    setIteamsNumber(10)
-  }
+  const handleAdd = () => {
+    setScroll(!scroll);
+    setIteamsNumber(10);
+  };
   const titleLang = [
     // ru
     {
@@ -34,32 +34,40 @@ const Reviews = ({ language, setLanguage }) => {
   return (
     <section className="py-16 bg-#040D12">
       <div className="containerb">
-        {titleLang.map((lan, index) => {
-          if (language === lan.lang) {
-            return (
-              <h2
-                key={index}
-                className="font-semibold leading-78px text-64px tracking-[-4%] text-white mb-8"
-              >
-                {lan.title}
-              </h2>
-            );
-          }
-        })}
-        <div className="flex justify-between">
-          <div className="py-2 px-4 bg-#9F95FF flex items-center  space-x-1.5 rounded-[22px] w-full max-w-[204px] h-full max-h-[50px]">
+        <div className="flex items-center justify-between  mb-8">
+          {titleLang.map((lan, index) => {
+            if (language === lan.lang) {
+              return (
+                <h2
+                  key={index}
+                  className="font-semibold leading-78px 1120px:text-64px 440px:text-4xl text-3xl tracking-[-4%] text-white"
+                >
+                  {lan.title}
+                </h2>
+              );
+            }
+          })}
+          <div className="950px:hidden py-2 440px:px-4 px-3 bg-#9F95FF flex items-center justify-center space-x-1.5 rounded-[22px]  h-full max-h-[50px]">
+            <img className="w-[16px] 440px:w-[32px]" src={smcImg} alt="Smc" />
+            <p className="440px:text-base text-xs leading-8 font-semibold text-white">
+              Оставить отзыв
+            </p>
+          </div>
+        </div>
+        <div className="flex 950px:justify-between justify-center">
+          <div className="950px:flex hidden py-2 px-4 bg-#9F95FF  items-center  space-x-1.5 rounded-[22px] w-full max-w-[204px] h-full max-h-[50px]">
             <img src={smcImg} alt="Smc" />
             <p className="text-base leading-8 font-semibold text-white">
               Оставить отзыв
             </p>
           </div>
-          <ul className={`space-y-7 h-[800px] ${scroll ? "reviews pr-5":""}`}>
+          <ul className={`space-y-7 h-[800px] ${scroll ? "reviews pr-5" : ""}`}>
             {HomeReviewsData.slice(0, iteamsNumber).map((lan, index) => {
               if (language === lan.lang) {
                 return (
                   <li
                     key={index}
-                    className="w-full 440px:max-w-780px max-w-440px py-4 px-7 border-white border rounded-20px "
+                    className="w-full 440px:max-w-780px max-w-440px  py-4 px-7 border-white border rounded-20px "
                   >
                     <div className="flex items-center mb-2.5">
                       <img
@@ -126,7 +134,10 @@ const Reviews = ({ language, setLanguage }) => {
           if (language === lan.lang) {
             return (
               <div key={index} className="flex items-end justify-end mt-7">
-                <button onClick={handleAdd} className="py-1.5 px-4 border-2 border-white rounded-[22px] font-semibold text-base leading-8 text-white ">
+                <button
+                  onClick={handleAdd}
+                  className="py-1.5 px-4 border-2 border-white rounded-[22px] font-semibold text-base leading-8 text-white "
+                >
                   {lan.dawload}
                 </button>
               </div>
