f643f761e7c969e36bb04b8b7bf71013627fe869
Javohir0080703 <renobobopocta@gmail.com>
2024-05-12 23:01:33 +0500

    fix responsive

--- src/pages/LastReporterDetail.jsx
+++ src/pages/LastReporterDetail.jsx
@@ -47,27 +47,27 @@ const LastReporterDetail = ({ language, setLanguage }) => {
             </div>
             <div>
               <h3 className="font-bold 531px:text-3xl text-xl 531px:leading-9 leading-6 tracking-[-1%] text-#9F95FF mb-4">{detail.why}</h3>
-              <p className="font-medium text-base leading-8 text-white ">{detail.whyText}</p>
+              <p className="font-medium 531px:text-base text-xs leading-8 text-white ">{detail.whyText}</p>
               <ul className="pl-12 mb-8">
                 {
-                  detail.aboutWhy.map((e, index)=>{return(<li className="list-number-style font-medium text-base leading-8 tracking-[-1%] text-white" key={index}>{e}</li>)})
+                  detail.aboutWhy.map((e, index)=>{return(<li className="list-number-style font-medium 531px:text-base text-xs leading-8 tracking-[-1%] text-white" key={index}>{e}</li>)})
                 }
               </ul>
             </div>
             <div>
               <h3 className="font-bold 531px:text-3xl text-xl 531px:leading-9 leading-6 tracking-[-1%] text-#9F95FF mb-4">{detail.etap}</h3>
-              <p  className="list-number-style font-medium text-base leading-8 tracking-[-1%] text-white">{detail.etapText}</p>
+              <p  className="list-number-style font-medium 531px:text-base text-xs leading-8 tracking-[-1%] text-white">{detail.etapText}</p>
             </div>
             <div>
               <h3 className="font-bold 531px:text-3xl text-xl 531px:leading-9 leading-6 tracking-[-1%] text-#9F95FF mb-4">{detail.kulayliklar}</h3>
-              <p  className="list-number-style font-medium text-base leading-8 tracking-[-1%] text-white">{detail.kulayliklarText}</p>
+              <p  className="list-number-style font-medium 531px:text-base text-xs leading-8 tracking-[-1%] text-white">{detail.kulayliklarText}</p>
             </div>
             <div className="flex justify-end">
              {
               Language.map((e, index)=>{
                 if(language === e.lang){
                   return(
-                    <Link key={index} to="/blog" className="py-1.5 px-4 font-semibold text-base leading-8 tracking-[-1%] text-white border-2 border-#CBC6FD rounded-[22px]">{e.title}</Link>
+                    <Link key={index} to="/blog" className="531px:py-1.5 531px:px-4 px-2.5 font-semibold 531px:text-base text-sm  leading-8 tracking-[-1%] text-white border-2 border-#CBC6FD rounded-[22px]">{e.title}</Link>
                   )
                 }
               })
