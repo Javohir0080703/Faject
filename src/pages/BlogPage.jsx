73f327c6899ca1a71d05d77a8a0b0d5eb9bd3fa6
Javohir0080703 <renobobopocta@gmail.com>
2024-05-14 23:16:24 +0500

    fix

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { lastReporterData } from "../data";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
-const BlogPage = () => {
+// import required modules
+import { Grid, Pagination } from "swiper/modules";
+import { Link } from "react-router-dom";
+const BlogPage = ({ language, setLanguage }) => {
+  const titleLang = [
+    // ru
+    {
+      title: "Блог",
+      lang: "ru",
+    },
+    // eng
+    {
+      title: "Блог",
+      lang: "eng",
+    },
+    // uzb
+    {
+      title: "Блог",
+      lang: "uzb",
+    },
+  ];
+
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
   return (
-    <div>BlogPage</div>
-  )
-}
+    <section className="py-16 bg-#040D12 select-none">
+      <div className="containerb">
+        {titleLang.map((e, index) => {
+          if (language === e.lang) {
+            return (
+              <h2 key={index} className="text-white 531px:text-64px text-5xl 531px:leading-78px leading-10 max-531px:text-center font-semibold tracking-[-4%] mb-8">
+                {e.title}
+              </h2>
+            );
+          }
+        })}
 
-export default BlogPage
+        <div className="">
+          <Swiper
+              spaceBetween={40}
+              slidesPerView={1}
+              breakpoints={{
+                880: {
+                  slidesPerView: 3,
+                  // slidesOffsetAfter:3,
+                  // centeredSlides: true,
+                },
+                590: {
+                  slidesPerView: 2,
+                  // centeredSlides: true,
+                },
+              }}
+            pagination={pagination}
+            modules={[Pagination]}
+            className="mySwiper relative  pb-[65px]"
+          > 
+            {lastReporterData.map((e, index) => {
+              if (language === e.lang) {
+                return (
+                  <SwiperSlide
+                    key={index}
+                    className="w-full 580px:max-w-[340px]  py-5 px-5 border-white border rounded-20px "
+                  >
+                    <Link to={`/last_reporter/${e.id}`}>
+                      <img className="mb-4 max-580px:w-full " src={e.img} alt={e.title} />
+                      <p className="text-white leading-8 text-base font-[300] mb-4">
+                        {e.data}
+                      </p>
+                      <p className="text-2xl leading-8 font-bold text-#9F95FF mb-1">
+                        {e.title}
+                      </p>
+                      <p className="font-medium text-base leading-8 text-white">
+                        {e.text.slice(0, 85)}...
+                      </p>
+                    </Link>
+                  </SwiperSlide>
+                );
+              }
+            })}
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BlogPage;
